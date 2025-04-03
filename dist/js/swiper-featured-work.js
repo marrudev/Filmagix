b4a193ce952825d03ba87f47cde243cd5e6bb49e
Martin Marusak <martin.marusak@protonmail.com>
2025-04-03 19:08:08 +0200

    HP Featured swiper responsive upd, Práce v cislech hide icons

--- dist/js/swiper-featured-work.js
+++ dist/js/swiper-featured-work.js
@@ -1,1 +1,1 @@
-var swiper=new Swiper(".swiper-featured-work",{slidesPerView:4,spaceBetween:16,pagination:{el:".swiper-pagination",clickable:!0}});
+var swiper=new Swiper(".swiper-featured-work",{slidesPerView:4,spaceBetween:16,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{320:{slidesPerView:1},560:{slidesPerView:2},900:{slidesPerView:3},1100:{slidesPerView:4}}});
--- src/js/swiper-featured-work.js
+++ src/js/swiper-featured-work.js
@@ -4,5 +4,19 @@ var swiper = new Swiper('.swiper-featured-work', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    560: {
+      slidesPerView: 2
+    },
+    900: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 4
+    }
   }
 });
